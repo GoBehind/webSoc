b316108ad5a6aa421b71aad7b9da28ff4e070d7e
Kuan <n789142536@hotmail.com>
2022-05-20 11:30:15 +0800

    Initial Commit

---
+++ WebSocketTest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WebSocketTest
+//
+//  Created by 王冠之 on 2022/5/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WebSocketTest/WebSocketTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebSocketTestApp.swift
+//  WebSocketTest
+//
+//  Created by 王冠之 on 2022/5/20.
+//
+
+import SwiftUI
+
+@main
+struct WebSocketTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
